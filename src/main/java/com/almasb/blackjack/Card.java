98d65ccf40e8638d4064f8a77567e579d52001e1
AlmasB <almaslvl@gmail.com>
2016-11-13 20:20:01 +0000

    added images

--- src/main/java/com/almasb/blackjack/Card.java
+++ src/main/java/com/almasb/blackjack/Card.java
@@ -1,9 +1,12 @@
 package com.almasb.blackjack;
 
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 /**
@@ -16,19 +19,33 @@ import javafx.scene.text.Text;
  */
 public class Card extends Parent {
 
+    private static final int CARD_WIDTH = 100;
+    private static final int CARD_HEIGHT = 140;
+
     enum Suit {
-        HEARTS, DIAMONDS, CLUBS, SPADES
-    };
+        HEARTS, DIAMONDS, CLUBS, SPADES;
+
+        final Image image;
+
+        Suit() {
+            this.image = new Image(Card.class.getResourceAsStream("images/".concat(name().toLowerCase()).concat(".png")),
+                    32, 32, true, true);
+        }
+    }
 
     enum Rank {
         TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10),
         JACK(10), QUEEN(10), KING(10), ACE(11);
 
         final int value;
-        private Rank(int value) {
+        Rank(int value) {
             this.value = value;
         }
-    };
+
+        String displayName() {
+            return ordinal() < 9 ? String.valueOf(value) : name().substring(0, 1);
+        }
+    }
 
     public final Suit suit;
     public final Rank rank;
@@ -39,15 +56,27 @@ public class Card extends Parent {
         this.rank = rank;
         this.value = rank.value;
 
-        Rectangle bg = new Rectangle(80, 100);
+        Rectangle bg = new Rectangle(CARD_WIDTH, CARD_HEIGHT);
         bg.setArcWidth(20);
         bg.setArcHeight(20);
         bg.setFill(Color.WHITE);
 
-        Text text = new Text(toString());
-        text.setWrappingWidth(70);
+        Text text1 = new Text(rank.displayName());
+        text1.setFont(Font.font(18));
+        text1.setX(CARD_WIDTH - text1.getLayoutBounds().getWidth() - 10);
+        text1.setY(text1.getLayoutBounds().getHeight());
 
-        getChildren().add(new StackPane(bg, text));
+        Text text2 = new Text(text1.getText());
+        text2.setFont(Font.font(18));
+        text2.setX(10);
+        text2.setY(CARD_HEIGHT - 10);
+
+        ImageView view = new ImageView(suit.image);
+        view.setRotate(180);
+        view.setX(CARD_WIDTH - 32);
+        view.setY(CARD_HEIGHT - 32);
+
+        getChildren().addAll(bg, new ImageView(suit.image), view, text1, text2);
     }
 
     @Override
